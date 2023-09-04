e653b29155a16114bb485ac2b66c897494b47e81
badmus360 <olashile.rahman@gmail.com>
2023-09-04 11:28:18 +0100

    the about us page is also adjusted along side with the home page

--- src/components/AboutTop.jsx
+++ src/components/AboutTop.jsx
@@ -90,7 +90,7 @@ const slides = {
           <div className='text-center text-[46px] sm:text-[64px] md:text-[79px] lg:text-[109px] mb-4 font-bold mt-6 xl:mb-6 xl:mt-8 xl:text-[70px]'><span className='text-red-500'>Who we are</span> <span className='text-yellow-500'>and</span> <span className='text-green-500'>what we do</span><span className='text-purple-500'>...</span></div>
               <div className='grid gap-10 sm:grid md:grid lg:grid justify-center group'>
 
-                  <div className='bg-black sm:gap-2 md:gap-3 w-[400px] h-[690px] sm:w-[600px] sm:h-[900px] md:w-[730px] md:h-[1000px] lg:w-[990px] lg:h-[450px] xl:w-[1150px] xl:h-[500px] grid sm:grid md:grid lg:grid-cols-2 bg-opacity-30 pb-3 p-2 sm:p-4 md:p-6 lg:p-6 xl:p-8 rounded-lg shadow-md'>
+                  <div className='bg-black sm:gap-2 md:gap-3 w-[380px] h-[690px] sm:w-[600px] sm:h-[900px] md:w-[730px] md:h-[1000px] lg:w-[990px] lg:h-[450px] xl:w-[1150px] xl:h-[500px] grid sm:grid md:grid lg:grid-cols-2 bg-opacity-30 pb-3 p-2 sm:p-4 md:p-6 lg:p-6 xl:p-8 rounded-lg shadow-md'>
     
                       <SlideShow images={slides.slides1} />
                       <div class="bg-blue-300 h-[300px] sm:h-[470px] md:h-[530px] lg:h-[400px] mt-1 sm:mt-0 md:mt-0 rounded-lg p-2">
@@ -99,7 +99,7 @@ const slides = {
                       </div>
                   </div>
   
-                 <div className='bg-black sm:gap-2 md:gap-3 w-[400px] h-[690px] gap-1 sm:w-[600px] sm:h-[900px] md:w-[730px] md:h-[1000px] lg:w-[990px] lg:h-[450px] xl:w-[1150px] xl:h-[500px] grid sm:grid md:grid lg:grid-cols-2 bg-opacity-30 b-5 p-2 sm:p-4 md:p-6 lg:p-6 xl:p-8 rounded-lg shadow-md'>
+                 <div className='bg-black sm:gap-2 md:gap-3 w-[380px] h-[690px] gap-1 sm:w-[600px] sm:h-[900px] md:w-[730px] md:h-[1000px] lg:w-[990px] lg:h-[450px] xl:w-[1150px] xl:h-[500px] grid sm:grid md:grid lg:grid-cols-2 bg-opacity-30 b-5 p-2 sm:p-4 md:p-6 lg:p-6 xl:p-8 rounded-lg shadow-md'>
                       <aside class="bg-green-300 h-[300px] sm:h-[470px] md:h-[530px] lg:h-[400px] mt-0 sm:mt-0 md:mt-0 rounded-lg p-2">
                               <span className='text-[30px] text-red-600 font-bold sm:text-[50px] md:text-[70px] lg:text-[38px] xl:text-[45px]'>...we are party shakers</span>
                               <p className='text-[22px] font-semibold sm:text-[36px] md:text-[41px] lg:text-3xl xl:text-4xl'>we've got a secret ingredient to make your parties shake with excitement! Imagine us as the musical magicians who bring the beats and rhythms that make your feet tap, your hips sway, and your heart dance. We're the ones with the coolest DJ music that's like a sprinkle of magic dust over your party</p>
@@ -108,7 +108,7 @@ const slides = {
                       <SlideShow images={slides.slides2} />
                   </div>
                   
-                  <div className='bg-black sm:gap-2 md:gap-3 w-[400px] h-[690px] sm:w-[600px] sm:h-[900px] md:w-[730px] md:h-[1000px] lg:w-[990px] lg:h-[450px] xl:w-[1150px] xl:h-[500px] grid sm:grid md:grid lg:grid-cols-2 bg-opacity-30 pb-3 p-2 sm:p-4 md:p-6 lg:p-6 xl:p-8 rounded-lg shadow-md'>
+                  <div className='bg-black sm:gap-2 md:gap-3 w-[380px] h-[690px] sm:w-[600px] sm:h-[900px] md:w-[730px] md:h-[1000px] lg:w-[990px] lg:h-[450px] xl:w-[1150px] xl:h-[500px] grid sm:grid md:grid lg:grid-cols-2 bg-opacity-30 pb-3 p-2 sm:p-4 md:p-6 lg:p-6 xl:p-8 rounded-lg shadow-md'>
 
                       <SlideShow images={slides.slides3} />
                       <aside class="bg-yellow-300 h-[300px] sm:h-[470px] md:h-[530px] lg:h-[400px] mt-1 sm:mt-0 md:mt-0 rounded-lg p-2">
@@ -117,7 +117,7 @@ const slides = {
                       </aside>
                   </div>
   
-                  <div className='bg-black sm:gap-2 md:gap-3 w-[400px] h-[690px] gap-1 sm:w-[600px] sm:h-[900px] md:w-[730px] md:h-[1000px] lg:w-[990px] lg:h-[450px] xl:w-[1150px] xl:h-[500px] grid sm:grid md:grid lg:grid-cols-2 bg-opacity-30 b-5 p-2 sm:p-4 md:p-6 lg:p-6 xl:p-8 rounded-lg shadow-md'>
+                  <div className='bg-black sm:gap-2 md:gap-3 w-[380px] h-[690px] gap-1 sm:w-[600px] sm:h-[900px] md:w-[730px] md:h-[1000px] lg:w-[990px] lg:h-[450px] xl:w-[1150px] xl:h-[500px] grid sm:grid md:grid lg:grid-cols-2 bg-opacity-30 b-5 p-2 sm:p-4 md:p-6 lg:p-6 xl:p-8 rounded-lg shadow-md'>
                       <aside class="bg-pink-300 h-[300px] sm:h-[470px] md:h-[530px] mt-0 sm:mt-0 md:mt-0 rounded-lg p-2">
                               <span className='text-[30px] text-red-600 font-bold sm:text-[50px] md:text-[70px] lg:text-[38px] xl:text-[45px]'>...we sell</span>
                               <p className='text-[22px] font-semibold sm:text-[36px] md:text-[41px] lg:text-3xl xl:text-4xl'>Are you ready to take a piece of an amusement park magic? "bringing the thrill of the amusement park right to you Imagine this: you're riding the happiest roller coasters, playing the most exciting games. Now, what if we told you that you can keep that excitement alive even after you've left the park.</p>
@@ -126,7 +126,7 @@ const slides = {
                       <SlideShow images={slides.slides4} />
                   </div>
   
-                  <div className='bg-black sm:gap-2 md:gap-3 w-[400px] h-[690px] sm:w-[600px] sm:h-[900px] md:w-[730px] md:h-[1000px] lg:w-[915px] lg:h-[400px] xl:w-[1150px] xl:h-[500px] grid sm:grid md:grid bg-opacity-30 pb-3 p-2 sm:p-4 md:p-6 lg:p-6 xl:p-8 rounded-lg shadow-md'>
+                  <div className='bg-black sm:gap-2 md:gap-3 w-[380px] h-[690px] sm:w-[600px] sm:h-[900px] md:w-[730px] md:h-[1000px] lg:w-[915px] lg:h-[400px] xl:w-[1150px] xl:h-[500px] grid sm:grid md:grid bg-opacity-30 pb-3 p-2 sm:p-4 md:p-6 lg:p-6 xl:p-8 rounded-lg shadow-md'>
 
                       <SlideShow images={slides.slides5} />
                       <aside class="bg-cyan-300 h-[300px] sm:h-[470px] md:h-[530px] mt-1 sm:mt-0 md:mt-0 rounded-lg p-2">
